2b0255d59055f52b398ba9fcdd022fb8d8911592
Federico Monteverdi <monteverdi.102@gmail.com>
2022-02-21 11:43:54 -0600

    added function that counts down from 10-1 and returns countdown phrase, added array with numbers associated with to countdownphrase, added variable countdownphrase

---
+++ countdown.js
@@ -0,0 +1,31 @@
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+let countdownPhrase = "Countdown ";
+
+const countDownNumbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function countDown() {
+    const para = document.createElement('p');
+    output.appendChild(para);
+    para.textContent =                  
+    for (let i = 10; i <= 0; i--) {
+        if (i > 0) {
+            countdownPhrase = `${countdownPhrase}${countDownNumbers[i]}`
+        } else {
+            countdownPhrase = "Blast off!"
+        }
+    }
+}
+
+console.log(countdownPhrase);
+
+
+
+
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
