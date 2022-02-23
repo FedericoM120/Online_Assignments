3e74a6f9e4a038681cfe2e7423bc80e4cbc3e365
Federico Monteverdi <monteverdi.102@gmail.com>
2022-02-22 23:04:12 -0600

    new assignemnt, loop sorts through array and adds users to admitted or refused list

---
+++ guestList.js
@@ -0,0 +1,38 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+for (let i = 0; i < people.length; i++) {
+    const admitted = document.querySelector('.admitted');
+    const refused = document.querySelector('.refused');
+
+    if (people === 'Lola' || 'Phil') {
+        refused.textContent = 'Refuse: '
+        refused.textContent += ;
+    } else {
+        admitted.textContent = 'Admit: ';
+        admitted.textContent += ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+// loop starts here
+
+// refused.textContent += ;
+// admitted.textContent += ;
